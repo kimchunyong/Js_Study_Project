33386715bc0b7fff2df8d811fb087ffeea9bb528
Kimchunyong <lovemy5854@naver.com>
2018-06-29 18:26:59 +0900

    add node parse

--- daily_coding/main.js
+++ daily_coding/main.js
@@ -4,17 +4,52 @@ var url = require("url");
 
 var app = http.createServer(function(request, response) {
   var _url = request.url;
-  console.log(_url);
   var queryData = url.parse(_url, true).query;
-  console.log(queryData.id);
+  var title = queryData.id;
+
   if (_url == "/") {
-    _url = "/index.html";
+    title = "Welcome";
   }
   if (_url == "/favicon.ico") {
     return response.writeHead(404);
   }
   response.writeHead(200);
   //console.log(__dirname + url);
-  response.end(queryData.id);
+
+  fs.readFile(`data/${queryData.id}`, "utf8", function(err, description) {
+    var template = `
+    <!doctype html>
+    <html>
+
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+
+    <body>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+      <ol>
+        <li>
+          <a href="/?id=HTML">HTML</a>
+        </li>
+        <li>
+          <a href="/?id=CSS">CSS</a>
+        </li>
+        <li>
+          <a href="/?id=Javascript">JavaScript</a>
+        </li>
+      </ol>
+      <h2>${title}</h2>
+      <p>
+        ${description}
+      </p>
+    </body>
+
+    </html>
+    `;
+    response.end(template);
+  });
 });
 app.listen(3000);
