b61e6a560015d68d3e5aa0d59937633e46cdfab8
Kimchunyong <lovemy5854@naver.com>
2018-10-17 19:10:34 +0900

    계산기 input값을 받고 출력하기 구현(연산의 길이에 상관없이 입력받음)

---
+++ calculator/calculator2.js
@@ -0,0 +1,42 @@
+"use strict";
+
+function main() {
+    var out = document.getElementById('output');
+    out.innerHTML = "계산된 값 출력:" + "<br/>";
+
+    var inp = document.getElementById("optTxt").value;
+
+    console.log(inp)
+    var op = [];
+    var num = [];
+    var count = 0;
+    var inp1;
+    var inp2;
+    var total;
+
+    for (var i = 0; i < inp.length; i++) {
+        total = Number(inp[0]);
+        if (i > 0) {
+            if (i % 2 === 1) {
+                op.push(inp[i]);
+            } else {
+                num.push(inp[i])
+            }
+        }
+    }
+
+    while (count <= num.length) {
+        inp2 = num[count];
+        if (op[count] === "+") {
+            total = total + Number(inp2);
+        } else if (op[count] === "-") {
+            total = total - Number(inp2);
+        } else if (op[count] === "*") {
+            total = total * Number(inp2);
+        } else if (op[count] === "/") {
+            total = total / Number(inp2);
+        }
+        count++;
+    }
+    out.innerHTML += total;
+}
