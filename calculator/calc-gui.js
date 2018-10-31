623945a59f3028ae65e89fa2462249d39b347081
Kimchunyong <lovemy5854@naver.com>
2018-10-31 13:10:49 +0900

    계산기 리스트에 넣기전에 맨처음 연산자가 나오면안됨,숫자를 입력한뒤 연산자가 연속으로 들어갈수없음. 진행중

--- calculator/calc-gui.js
+++ calculator/calc-gui.js
@@ -1,2 +1,24 @@
 'use strict';
 
+var calc = {};
+
+calc.list = '';
+
+calc.click = function (e) {
+    var target = e.target;
+    var val = target.textContent
+    this.pushList(val);
+}
+
+calc.pushList = function (val) {
+    console.log(val);
+
+    this.list += val;
+    console.log(this.list)
+    console.log(this.list[this.list.length - 1])
+}
+
+var ele = document.getElementById('calc_wrap');
+ele.addEventListener('click', function (e) {
+    calc.click(e);
+})
