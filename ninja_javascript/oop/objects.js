96007861fe670e227eca55f63fd7d748e637fc33
Kimchunyong <lovemy5854@naver.com>
2018-11-06 18:32:10 +0900

    e-mail 관련 모델링 연습

--- ninja_javascript/oop/objects.js
+++ ninja_javascript/oop/objects.js
@@ -1,13 +1,34 @@
+/*
 var userOne = {
     email: 'ryu@ninjas.com',
     name: 'Ryu',
+
     login() {
-        console.log(this.email, 'has logged in');
+        console.log(`${this.email}, has logged in`);
     },
+
     logout() {
-        console.log(this.email, 'has logged out');
+        console.log(`${this.email}, has logged out`);
     }
 };
+*/
 
-userOne.login();
-userOne.logout();
+class UserInfo {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    login() {
+        console.log(`${this.email}, has logged in`);
+    }
+
+    logout() {
+        console.log(`${this.email}, has logged out`);
+    }
+}
+
+var userOne = new UserInfo('ryu@ninjas.com', 'Ryu');
+
+console.log(userOne.login());
+console.log(userOne.logout());
