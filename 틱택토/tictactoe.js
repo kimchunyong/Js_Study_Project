70b580b4be7d934de2c862fa9b11734a7d545593
Kimchunyong <lovemy5854@naver.com>
2018-11-30 17:06:35 +0900

    2차원 배열 이용하여 몇줄 몇칸 인지 알아내기

--- 틱택토/tictactoe.js
+++ 틱택토/tictactoe.js
@@ -1,26 +1,39 @@
 var wrap = document.createElement('div');
 wrap.classList = 'wrap';
+var flag = true;
+var baseArr = [];
+var AllchkArr = [];
+var rows = [];
+var cols = [];
+var clickArr = [];
 
 for (var i = 0; i < 3; i++) {
     var rowItem = document.createElement('div');
     rowItem.classList = 'row-item';
+    rows.push(rowItem);
+    cols.push([]);
+    AllchkArr.push([]);
     for (var j = 0; j < 3; j++) {
         var col = document.createElement('div');
         col.classList = 'col';
+        cols[i].push(col);
+        AllchkArr[i].push(col);
         rowItem.append(col);
     }
     wrap.append(rowItem);
 }
 document.body.append(wrap);
 
-var flag = true;
-var baseArr = []
-var clickArr = [];
 
 var col = document.querySelectorAll('.col');
 col.forEach(function (el) {
     baseArr.push(el);
-    el.addEventListener('click', function () {
+    el.addEventListener('click', function (e) {
+        var whatRow = rows.indexOf(e.target.parentNode);
+        console.log("몇줄", whatRow);
+        var whatCol = cols[whatRow].indexOf(e.target);
+        console.log('몇칸', whatCol);
+
         baseArr.splice(baseArr.indexOf(el), 1);
         if (!clickArr.includes(el)) {
             clickArr.push(el);
@@ -32,10 +45,9 @@ col.forEach(function (el) {
                     baseArr[rndIdx].textContent = 'O';
                     baseArr.splice(rndIdx, 1);
                     flag = true;
-                }, 500)
+                }, 100)
             }
         }
     })
 })
-
 
