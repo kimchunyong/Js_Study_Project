8fdbb2706195553049f0c3f876d36f839a32eb19
Kimchunyong <lovemy5854@naver.com>
2018-12-03 17:01:56 +0900

    setTimeout함수 실행시 클로저 현상이 생김 IEFF함수를 활용하여 새로운  실행컨텍스트를 생성하여 해결

--- 로또추첨기/lotto.js
+++ 로또추첨기/lotto.js
@@ -17,3 +17,26 @@ var resultNum = shuffle.slice(0, 6).sort(function (a, b) {
     return a - b;
 })
 
+var result = resultNum.concat(bonus);
+var resultWrap = document.getElementById('result-wrap');
+
+
+for (var i = 0; i < result.length; i++) {
+    (function (i) {
+        var current = i;
+        setTimeout(function () {
+            var circle = document.createElement('div');
+            circle.textContent = result[i];
+            circle.style.width = "30px";
+            circle.style.height = "30px";
+            circle.style.lineHeight = "30px";
+            circle.style.textAlign = "center";
+            circle.style.border = "1px solid #000";
+            circle.style.borderRadius = "50%";
+            resultWrap.append(circle);
+        }, current * 1000)
+    })(i)
+
+}
+
+
