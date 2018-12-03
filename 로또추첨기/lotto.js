340ffeaf778756655ef3335d3044741bca147849
Kimchunyong <lovemy5854@naver.com>
2018-12-03 16:11:07 +0900

    랜덤 + 보너스 번호

--- 로또추첨기/lotto.js
+++ 로또추첨기/lotto.js
@@ -1,5 +1,17 @@
+
 var lottoNum = Array(45)
     .fill()
     .map(function (item, idx) {
         return idx + 1;
     });
+
+
+var shuffle = [];
+while (lottoNum.length > 0) {
+    var mvNum = lottoNum.splice(Math.floor(Math.random() * lottoNum.length), 1)[0]
+    shuffle.push(mvNum);
+}
+
+var bonus = shuffle[shuffle.length - 1];
+var resultNum = shuffle.slice(0, 6);
+
