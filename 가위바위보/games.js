11d1097ec6934efb1ff94e579afdb7d1f336e786
Kimchunyong <lovemy5854@naver.com>
2018-12-04 18:05:08 +0900

    버튼 클릭시 컴퓨터 멈추고 현재의 값 추출, 이제 내가 낸 값과 비교해서 승리자 선언

--- 가위바위보/games.js
+++ 가위바위보/games.js
@@ -1,7 +1,6 @@
 var computer = document.getElementsByClassName('computer')[0];
-
 var left = 0;
-setInterval(function () {
+var computerImg = setInterval(function () {
     if (left === 0) {
         left = -142;
     } else if (left === -142) {
@@ -13,3 +12,26 @@ setInterval(function () {
 }, 300)
 
 /*버튼을 눌렀을때 멈춰있는  position의 값을 뽑아서 가위,바위,보 인지 구분후 누가 이겼는지 결정*/
+
+var btns = document.querySelectorAll('button');
+var flag = false;
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        if (!flag) {
+            var btn = e.target.textContent;
+            var stopCmpTxt;
+            var userTxt
+            clearInterval(computerImg);
+            console.log(btn)
+            if (left === 0) {
+                stopCmpTxt = "바위"
+            } else if (left === -142) {
+                stopCmpTxt = "가위"
+            } else {
+                stopCmpTxt = "보"
+            }
+            flag = true;
+        }
+    })
+
+})
