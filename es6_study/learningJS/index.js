03678ff78e4b7ab8a5cc2128116c28ae148173ee
kimchunyong <lovemy5854@naver.com>
2019-03-08 22:07:19 +0900

    DOM API

--- es6_study/learningJS/index.js
+++ es6_study/learningJS/index.js
@@ -1,11 +1,20 @@
-alert('hellow world');
+// alert('hellow world');
 /*
 let a = 221;
 let b = a - 5;
 a = 4;
 */
-
+/*
 const a = 221;
 const b = a - 5;
 a = 4; // error
 console.log(b, a);
+*/
+
+const title = document.querySelector('h1');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function (e) {
+  e.preventDefault();
+  title.innerHTML = 'Change text';
+});
